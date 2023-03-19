948759ff9c2e7313b41776e79c830a58849956a5
Cedric Joulain <cedric.joulain@kereon-intelligence.com>
2023-03-19 17:24:23 +0100

    ignore: comments

--- position.go
+++ position.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+// From https://github.com/theOehrly/Fast-F1.git api.py
+// Status (str): 'OnTrack' or 'OffTrack'
+// X, Y, Z (int): Position coordinates; starting from 2020 the coordinates are given in 1/10 meter
+
 type OnePosition struct {
 	Status string // TODO enum: seen OnTrack only
 	X      int
